c7c2f165211ced5178d0ff9baba67baea0a7174b
pluto <mayuanchi1029@gmail.com>
2023-03-07 08:35:22 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import path from 'path';
+
+function main() {
+  console.log(path);
+}
+
+main();
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true
+          }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: { node: 'current' } }],
+              '@babel/preset-typescript'
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
